f4cf5a16ea42c63511b23c8d8ff2ebe22e2ffd78
Jeremy Laningham <JeremyLaningham@gmail.com>
2020-03-12 16:49:18 -0500

    solutions

---
+++ app-1/src/App.js
@@ -0,0 +1,49 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userInput: ""
+    };
+  }
+
+  handleChange(value) {
+    this.setState({ userInput: value });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input onChange={e => this.handleChange(e.target.value)} type="text" />
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+//function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
---
+++ app-5/src/Component/Image.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Image extends React.Component {
+    render() {
+        return (
+            <div>
+                <img width="700" height="500" src="https://mydoodlebabies.com/wp-content/uploads/2018/03/Kramer.png"/>
+                <div>Dogs are better than cats</div>
+            </div>
+        )
+    }
+}
+
+export default Image
